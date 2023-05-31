0be5af2324c0d7129055a6343a1d9a258f61bf36
Hristo Pisarev <102079784+Pisarevv@users.noreply.github.com>
2023-05-31 22:23:27 +0300

    Initialization and post model created

---
+++ Workshop - Forum App/ForumApp/Infrastructure/Configuration/PostEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace Forum.Infrastructure.Configuration
+{
+    using Forum.Infrastructure.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using System;
+
+    internal class PostEntityConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Workshop - Forum App/ForumApp/Infrastructure/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿namespace Forum.Infrastructure.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Forum.Common.Validation.ValidationConstants.Post;
+    public class Post
+    {
+        public Post()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(MinTitleLenght)]
+        [MaxLength(MaxTitleLenght)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MinLength(MinTitleLenght)]
+        [MaxLength(MaxTitleLenght)]
+        public string Content { get; set; } = null!;
+    }
+}
