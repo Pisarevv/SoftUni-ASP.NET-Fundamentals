13f3a88bb2bf335cfe621e19dde610d861b847b7
Hristo Pisarev <102079784+Pisarevv@users.noreply.github.com>
2023-06-10 19:28:48 +0300

    Created base entities

---
+++ Workshop - TaskBoard App/TaskBoard/TaskBoard.Infrastructure/Data/Models/Board.cs
@@ -0,0 +1,25 @@
+﻿namespace TaskBoard.Infrastructure.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static TaskBoard.Common.ValidationConstants.Board;
+    public class Board
+    {
+        public Board()
+        {
+            this.Id = Guid.NewGuid();
+            this.Tasks = new HashSet<Task>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(MinNameLength)]
+        [MaxLength(MaxNameLength)]
+        public string Name { get; set; } = null!;
+
+        public IEnumerable<Task> Tasks { get; set; }
+
+    }
+}
---
+++ Workshop - TaskBoard App/TaskBoard/TaskBoard.Infrastructure/Data/Models/Task.cs
@@ -0,0 +1,38 @@
+﻿namespace TaskBoard.Infrastructure.Data.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static TaskBoard.Common.ValidationConstants.Task;
+    public class Task
+    {
+        public Task()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(MinTitleLength)]
+        [MaxLength(MaxTitleLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MinLength(MinDescriptionLength)]
+        [MaxLength(MaxDescriptionLength)]
+        public string Description { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        [ForeignKey(nameof(Board))]
+        public int? BoardId { get; set; }
+
+        public Board? Board { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Owner))]
+        public string OwnerId { get; set; } = null!;
+
+        public IdentityUser Owner { get; set; } = null!;
+    }
+}
