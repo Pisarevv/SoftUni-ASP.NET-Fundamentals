aea132854c7a98d1b8a916eed02225aa19707701
Hristo Pisarev <102079784+Pisarevv@users.noreply.github.com>
2023-06-10 19:07:41 +0300

    Project clean up
    Remove of unnecessary components in identity views

--- Workshop - TaskBoard App/TaskBoard/TaskBoard.Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Workshop - TaskBoard App/TaskBoard/TaskBoard.Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,9 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using System;
-using TaskBoard.Infrastructure.Data;
 
 namespace TaskBoard.Infrastructure.Data.Migrations
 {
