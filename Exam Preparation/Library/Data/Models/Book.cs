220e5ae41f85686bf029e06b6db52cbcb63c1e2e
Hristo Pisarev <102079784+Pisarevv@users.noreply.github.com>
2023-06-12 21:53:49 +0300

    Project initialization, added base entities
    Added entities with validations

---
+++ Exam Preparation/Library/Data/Models/Book.cs
@@ -0,0 +1,42 @@
+﻿namespace Library.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static Common.ValidationConstants.Book;
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(MinTitleLength)]
+        [MaxLength(MaxTitleLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MinLength(MinAuthorNameLength)]
+        [MaxLength(MaxAuthorNameLength)]
+        public string Author { get; set; }  = null!;
+
+        [Required]
+        [MinLength(MinDescriptionLength)]
+        [MaxLength(MaxDescriptionLength)]
+        public string Description { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Range(MinRating,MaxRating)]
+        public decimal Rating { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public Category Category { get; set; } = null!;
+
+        public IdentityUserBook UsersBooks { get; set; } = null!;
+    }
+}
