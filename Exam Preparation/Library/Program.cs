55dcb02005395c37bd499fcee4249c69e942938d
Hristo Pisarev <102079784+Pisarevv@users.noreply.github.com>
2023-06-13 22:25:33 +0300

    Configurated identity password options

--- Exam Preparation/Library/Program.cs
+++ Exam Preparation/Library/Program.cs
@@ -10,9 +10,17 @@ builder.Services.AddDbContext<LibraryDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<LibraryDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.SignIn.RequireConfirmedAccount = false;
+});
 
 var app = builder.Build();
 
