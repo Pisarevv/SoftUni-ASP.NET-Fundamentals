10a9e6d31ffeb8657ea0ed06425af1567631026a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-06-14 15:16:02 +0300

    Created models for add book page

---
+++ Exam Preparation/Library/Models/BookFormViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Library.Models;
+using System.ComponentModel.DataAnnotations;
+using static Common.ValidationConstants.Book;
+
+public class BookFormViewModel
+{
+    public BookFormViewModel()
+    {
+         this.Categories = new List<CategoryViewModel>();
+    }
+
+
+    [Required]
+    [MinLength(MinTitleLength)]
+    [MaxLength(MaxTitleLength)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [MinLength(MinAuthorNameLength)]
+    [MaxLength(MaxAuthorNameLength)]
+    public string Author { get; set; } = null!;
+
+    [Required]
+    [MinLength(MinDescriptionLength)]
+    [MaxLength(MaxDescriptionLength)]
+    public string Description { get; set; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public string Url { get; set; } = null!;
+
+    [Required]
+    public string Rating { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+
+    public ICollection<CategoryViewModel> Categories { get; set; } 
+
+}
